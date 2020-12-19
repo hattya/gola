c6ee61630456c46cda9e454c1cdf3c8837189a9b
Akinori Hattori <hattya@gmail.com>
2020-12-19 21:21:26 +0900

    Improve tests

--- gola_test.go
+++ gola_test.go
@@ -19,6 +19,168 @@ import (
 	"testing"
 )
 
+func TestNewGola(t *testing.T) {
+	dir, err := tempDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	argv0 := filepath.Join(dir, "gola")
+	json := filepath.Join(dir, "gola.json")
+	exe := argv0
+	switch runtime.GOOS {
+	case "windows":
+		exe += ".exe"
+	}
+	if err := ioutil.WriteFile(exe, []byte{}, 0777); err != nil {
+		t.Fatal(err)
+	}
+	name := filepath.Join(dir, "a")
+	if err := os.Mkdir(name, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := file(filepath.Join(name, "__main__.py"), "#!/usr/bin/env python\n"); err != nil {
+		t.Fatal(err)
+	}
+	// invalid config
+	if err := file(json, ""); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := newGola(argv0, name); err == nil {
+		t.Error("expected error")
+	}
+	// not found
+	if err := file(json, `{}`); err != nil {
+		t.Fatal(err)
+	}
+	if _, err := newGola(argv0, name); err == nil {
+		t.Error("expected error")
+	}
+	// redirect
+	if err := file(json, `{"dir": ["__main__.py"]}`); err != nil {
+		t.Fatal(err)
+	}
+	if g, err := newGola(argv0, name); err != nil {
+		t.Error(err)
+	} else if g, e := g.name, filepath.Join(name, "__main__.py"); g != e {
+		t.Errorf("expected %q, got %q", e, g)
+	}
+}
+
+func TestLoadConfig(t *testing.T) {
+	dir, err := tempDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	path := os.Getenv("PATH")
+	defer os.Setenv("PATH", path)
+	os.Setenv("PATH", dir+string(os.PathListSeparator)+path)
+
+	var argv0 string
+	switch runtime.GOOS {
+	case "windows":
+		argv0 = filepath.Join(dir, "gola.exe")
+	default:
+		argv0 = filepath.Join(dir, "gola")
+	}
+	json := filepath.Join(dir, "gola.json")
+
+	g := &gola{}
+	if err := g.loadConfig(argv0); err != nil {
+		t.Error(err)
+	}
+	if err := g.loadConfig(filepath.Base(argv0)); err == nil {
+		t.Error("expected error")
+	}
+	// invalid config
+	if err := ioutil.WriteFile(argv0, []byte{}, 0777); err != nil {
+		t.Fatal(err)
+	}
+	if err := file(json, ""); err != nil {
+		t.Fatal(err)
+	}
+	// :: abs
+	if err := g.loadConfig(argv0); err == nil {
+		t.Error("expected error")
+	}
+	// :: rel
+	popd, err := pushd(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := g.loadConfig(filepath.Base(argv0)); err == nil {
+		t.Error("expected error")
+	}
+	if err := popd(); err != nil {
+		t.Fatal(err)
+	}
+	// :: lookup PATH
+	if err := g.loadConfig(filepath.Base(argv0)); err == nil {
+		t.Error("expected error")
+	}
+	// valid config
+	if err := file(json, "{}"); err != nil {
+		t.Fatal(err)
+	}
+	// :: abs
+	if err := g.loadConfig(argv0); err != nil {
+		t.Error(err)
+	}
+	// :: rel
+	popd, err = pushd(dir)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := g.loadConfig(filepath.Base(argv0)); err != nil {
+		t.Error(err)
+	}
+	if err := popd(); err != nil {
+		t.Fatal(err)
+	}
+	// :: lookup PATH
+	if err := g.loadConfig(filepath.Base(argv0)); err != nil {
+		t.Error(err)
+	}
+}
+
+func TestExec(t *testing.T) {
+	dir, err := tempDir()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
+	stdout := os.Stdout
+	defer func() { os.Stdout = stdout }()
+	os.Stdout = nil
+
+	g := &gola{}
+	if err := g.exec([]string{}); err == nil {
+		t.Error("expected error")
+	}
+
+	g.name = filepath.Join(dir, "a.go")
+	g.ext = filepath.Ext(g.name)
+	if err := file(g.name, "#!/usr/bin/env go\n"); err != nil {
+		t.Fatal(err)
+	}
+	if err := g.exec([]string{}); err == nil {
+		t.Error("expected error")
+	}
+
+	g.config.Map = map[string]map[string]string{
+		"go": {
+			"": "go",
+		},
+	}
+	if err := g.exec([]string{"version"}); err != nil {
+		t.Error(err)
+	}
+}
+
 func TestLoadScript(t *testing.T) {
 	dir, err := tempDir()
 	if err != nil {
@@ -189,4 +351,15 @@ func file(name, data string) error {
 
 func touch(name string) error {
 	return ioutil.WriteFile(name, []byte{}, 0666)
+}
+
+func pushd(path string) (func() error, error) {
+	wd, err := os.Getwd()
+	popd := func() error {
+		if err != nil {
+			return err
+		}
+		return os.Chdir(wd)
+	}
+	return popd, os.Chdir(path)
 }
